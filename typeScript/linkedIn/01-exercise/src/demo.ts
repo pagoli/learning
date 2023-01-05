2c31a0367fc3d4fdfa979c32e7cba8a4404cab81
pagoli <oliviapagel89@gmail.com>
2023-01-05 15:55:29 +0100

    LinkedIn Course TS Notes

--- typeScript/linkedIn/01-exercise/src/demo.ts
+++ typeScript/linkedIn/01-exercise/src/demo.ts
@@ -1,5 +1,52 @@
-// class Contact {
-//     id;
-//     name;
-//     birthDate;
-// }
+// $ CUSTOM TYPES => Interfaces
+//  => similar to "Classes"
+//
+//  => Difference:
+// Interfaces only exist before compilation
+// to provide type information to TS;
+//  will never be available on runtime code
+
+interface Contact extends Address {
+  // extends Address makes Address fields available in Contact interface
+  id: number;
+  name: ContactName;
+  birthDate?: Date; // ? => OPTIONAL
+  status?: ContactStatus;
+}
+
+interface Address {
+  line1?: string;
+  line2?: string;
+  province?: string;
+  region?: string;
+  postalCode?: number;
+}
+
+// & TYPE ALIAS
+// => provides an alias for an already existing type;
+
+type ContactName = string; // replaces here the "name" inside of Contact interface
+
+// & ENUMERABLE TYPES
+// & special Type of values with hard coded list of values
+//  => i.e. for statuses (active, inactive, new...)
+
+enum ContactStatus {
+  Active = "active",
+  Inactive = "inactive",
+  New = "new",
+}
+
+let primaryContact: Contact = {
+  birthDate: new Date("01-01-2015"),
+  id: 23456,
+  name: "Hon Duras",
+  postalCode: 123456,
+  status: ContactStatus.Inactive,
+};
+
+function clone(source: Contact) {
+  return Object.apply({}, source);
+}
+
+const a: Contact = { id: 123, name: "Homer S" };
---
+++ typeScript/linkedIn/01-exercise/src/demo2.ts
@@ -0,0 +1,10 @@
+// interface Contact {
+//   id: number;
+//   name: string;
+// }
+
+// function clone(source: Contact) {
+//   return Object.apply({}, source);
+// }
+
+// const a: Contact = { id: 123, name: "Homer S" };
